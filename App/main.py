3542e8e5d4831244267bab7e44a855b681ca80f9
Lin-Ian <ianrp.lin@gmail.com>
2022-10-21 15:29:35 -0400

    Classify messages through web app

--- App/main.py
+++ App/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from Classify import *
+import joblib
 
 app = Flask(__name__)
 
@@ -10,10 +12,20 @@ def home():
 
 @app.route("/result", methods=['POST', 'GET'])
 def result():
+    # Get message from HTML form
     message = request.form["message"]
+
+    # Print classification (spam/ham) to console
+    print(classify(message, vectorize, random_forest_model))
 
     return render_template("home.html", status=message)
 
 
 if __name__ == "__main__":
+    # Load Vectorizer
+    vectorize = joblib.load('Model/tfidf_vect_fit.pkl')
+    # Load random forest model
+    random_forest_model = joblib.load('Model/rf_model.pkl')
+
     app.run(debug=True)
+
